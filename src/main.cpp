ce11663232340cf3593cd79fad8bf59eb98bb2fc
Akil Darjean <adarjean@uh.edu>
2017-06-12 18:36:52 -0500

    Add main.cpp source file

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2017 Akil Darjean (adarjean@uh.edu)
+ * Distributed under the MIT License.
+ * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+ */
+
+int main(int argc, char const* argv[])
+{
+    return 0;
+}
